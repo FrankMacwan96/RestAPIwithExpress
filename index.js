9086edcef58bf35c0ef73b834d961dc722e29f41
Frank <macwanfrank96@gmail.com>
2020-08-31 20:40:34 -0400

    front end code added

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const parser = require('body-parser');
 const app = express();
+const cors = require('cors');
 require('dotenv/config');
 
 app.use(parser.json());
+app.use(cors())
+
 //importing routes
 const postroute = require('./Posts/posts');
 
@@ -16,7 +19,7 @@ app.get('/',(req,res)=>{
 });
 
 
-const db = process.env.DB_CONN
-const port = process.env.PORT || 3000
-mongoose.connect(db,{useNewURLParser:true},()=>console.log("COnnected to DB.."))
-app.listen(port, () => console.log('listnening on port 3000...'))
+const db = process.env.DB_CONN;
+const port = process.env.PORT || 3000;
+mongoose.connect(db,{useNewURLParser:true},()=>console.log("Connected to DB.."));
+app.listen(port, () => console.log('listnening on port 3000...'));
